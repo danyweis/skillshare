95eac74c7d447a914a2c75509feb1b23e6d183fa
danyweis <danyweismail@gmail.com>
2020-05-12 08:18:48 +0100

    functions

---
+++ ModernJS/function/fibanacci.js
@@ -0,0 +1,38 @@
+
+
+let fibonacci = (len) => {
+    let num1 = 0
+    let num2 = 1
+    let toPush = 0
+    const arr = []
+    len-- // do -- because the first time we push 2 numbers
+    for (let i =0; i < len; i++){
+        if (toPush === 0){
+            arr.push(num1,num2)
+            toPush = num1 + num2
+            num1 = num2
+            num = toPush
+            continue;
+        }
+        arr.push(toPush)
+        toPush = num1 + num2
+        num1 = num2
+        num2 = toPush
+    }    
+    console.log(arr)
+}
+fibonacci(10)
+
+
+// other way
+
+let fibSeq = (len) => {
+
+    let seq = [0,1]
+    
+    for (let i = 2; i < len; i++){
+        seq[i] = seq[i-1] + seq[i-2]
+    }
+    console.log(seq)
+}
+fibSeq(7)
---
+++ ModernJS/function/oddEven.js
@@ -0,0 +1,5 @@
+//  ODD EVEN CHECKER
+
+let oddeven = (num) => (num % 0) ? `${num} in an even Number` : `${num} in an odd Number`;
+
+console.log(oddeven(8))
