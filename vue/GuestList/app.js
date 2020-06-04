1a8eb02c5092167aabf49738a33b4afe2087a6ca
danyweis <danyweismail@gmail.com>
2020-06-04 18:29:05 +0100

    vue js still learning

--- vue/GuestList/app.js
+++ vue/GuestList/app.js
@@ -1,4 +1,4 @@
-new Vue({
+var vm1 = new Vue({
     el: "#app",
     data: {
         event: {
@@ -8,19 +8,49 @@ new Vue({
         },
         newNameText: "",
         guestName: [],
-        appStyle: {
-            marginTop: "25px",
-        },
         eventCapacity: 25,
         eventCapacityPercentage: 0,
     },
     methods: {
         formSubmitted: function () {
             if (this.newNameText.length > 0 && this.eventCapacityPercentage < 100) {
-                this.guestName.push(this.newNameText);
+                this.guestName.push(this.newNameText.toLowerCase());
                 this.newNameText = "";
                 this.eventCapacityPercentage = this.guestName.length / (this.eventCapacity / 100);
             }
+        },
+    },
+    computed: {
+        sortNames: function () {
+            return this.guestName.sort();
+        },
+    },
+    watch: {
+        guestName: function (data) {
+            console.log("guestName is triggered");
+        },
+    },
+    filters: {
+        formatName: function (value) {
+            return value.slice(0, 1).toUpperCase() + value.slice(1).toLowerCase();
+        },
+    },
+});
+
+var vm2 = new Vue({
+    el: "#navigation",
+    data: {
+        appName: "Guest List",
+        navLinks: [
+            { name: "Home", id: 1, url: "https://vuejs.org/v2/guide/" },
+            { name: "Upcoming Events", id: 2, url: "https://www.google.com" },
+            { name: "Guest Benefits", id: 3, url: "https://www.google.com" },
+            { name: "Latest News", id: 4, url: "https://www.google.com" },
+        ],
+    },
+    methods: {
+        changeCapacity: function () {
+            vm1.eventCapacity = 15;
         },
     },
 });
